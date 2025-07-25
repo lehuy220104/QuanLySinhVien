56b03624d945e20c7f3a0f69411aff768e9a7eff
Huy Le <daithanhmb3@gmail.com>
2025-07-25 23:16:08 +0700

    first commit

---
+++ QuanLySinhVien/DAL_QuanLy/DBConnect.cs
@@ -0,0 +1,9 @@
+﻿using System.Data.SqlClient;
+
+namespace DAL_QuanLy
+{
+    public class DBConnect
+    {
+        protected SqlConnection _conn = new SqlConnection("Data Source=LAPTOP-GC2FQGCT\\SQLEXPRESS;Initial Catalog=QLSinhVien;Integrated Security=True");
+    }
+}
---
+++ QuanLySinhVien/DTO_QuanLy/DTO_TaiKhoan.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace DTO_QuanLy
+{
+    public class DTO_TaiKhoan
+    {
+        private string _TENDANGNHAP;
+        private string _MATKHAU;
+        private string _VAITRO;
+
+        /* ************* GETTER/SETTER ************* */
+        public string TENDANGNHAP
+        {
+            get { return _TENDANGNHAP; }
+            set { _TENDANGNHAP = value; }
+        }
+
+        public string MATKHAU
+        {
+            get { return _MATKHAU; }
+            set { _MATKHAU = value; }
+        }
+
+        public string VAITRO
+        {
+            get { return _VAITRO; }
+            set { _VAITRO = value; }
+        }
+
+        /* === Constructor === */
+        public DTO_TaiKhoan() { }
+
+        public DTO_TaiKhoan(string tenDangNhap, string matKhau, string vaiTro)
+        {
+            this.TENDANGNHAP = tenDangNhap;
+            this.MATKHAU = matKhau;
+            this.VAITRO = vaiTro;
+        }
+    }
+}
